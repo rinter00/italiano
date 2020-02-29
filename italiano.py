338e59ced1ada05540e478cc775b4a4051129f66
rin <61584955+rinter00@users.noreply.github.com>
2020-02-29 14:38:41 +0900

    Update italiano.py

--- italiano.py
+++ italiano.py
@@ -1,1 +1,13 @@
-Italiano.ipynb
+sub = ['io','tu','lui','lei','noi','voi','loro']
+essere = {'io':'sono','tu':'sei','lui':'e','lei':'e','noi':'siamo','voi':'siete','loro':'sono'}
+from random import randint
+
+while True:
+    subj = sub[randint(0, 6)]
+    answer = input('Conjugate essere to {}'.format(subj))
+    if answer == 'q':
+        break
+    elif answer == essere[subj]:
+        print('Correct!')
+    else :
+        print('Oups! The answer is {}'.format(essere[subj]))
